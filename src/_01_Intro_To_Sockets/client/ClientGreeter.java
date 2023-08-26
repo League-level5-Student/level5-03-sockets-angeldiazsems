33a28150f069ab39cff1eb2f029337e0a1197306
student <student@ip-172-31-48-186.us-west-2.compute.internal>
2023-08-26 11:09:44 -0700

    s

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -1,6 +1,9 @@
 package _01_Intro_To_Sockets.client;
 
 import java.net.*;
+
+import com.sun.security.ntlm.Server;
+
 import java.io.*;
 
 public class ClientGreeter {
@@ -8,13 +11,18 @@ public class ClientGreeter {
    public static void main(String [] args) {
 	  //1. Create a String for the ip address of the server. 
 	  // If you don't know how to find a computer's ip address, ask about ifconfig on linux/mac and ipconfig on windows.
-      
+	   
+	   
+	   String ip = "s";
+	   
       //2. Create an integer for the server's port number
       
+	   int portNum = 8080;
+	   
       //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
     
     	 //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
- 
+	    Socket s = new Socket(ip, portNum);
          //5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
          
          //6. Use the DataOutputStream object to send a message to the server using the writeUTF(String message) method
